49151fab2fc746973bf2db189cdf13b30d7dd46b
Brian Preston <brian8181@gmail.com>
2021-02-26 18:04:19 -0600

    update files

--- build_cache.sh
+++ build_cache.sh
@@ -6,8 +6,3 @@ PROJECT_PATH='/home/brian/src/music_shell'
 find "$MUSIC_PATH" -iname "*.mp3" > $PROJECT_PATH/music_cache.txt
 find "$MUSIC_PATH" -iname "*.ogg" >> $PROJECT_PATH/music_cache.txt
 find "$MUSIC_PATH" -iname "*.flac" >> $PROJECT_PATH/music_cache.txt
-
-
-# music cache
-#find . -iname *.mp3 > ~/script/music_cache.txt
-#cat ~/script/music_cache.txt
