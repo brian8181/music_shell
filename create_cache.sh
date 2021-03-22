d116a2415574b1df92ec4028b131058feb7a7d57
Brian K <brian8181@gmail.com>
2021-03-22 08:54:24 -0500

    created mccache.txt

--- create_cache.sh
+++ create_cache.sh
@@ -7,3 +7,9 @@ NAME=$3
 find $MUSIC_PATH -iname "*.mp3" > $PROJECT_PATH/$NAME
 find $MUSIC_PATH -iname "*.ogg" >> $PROJECT_PATH/$NAME
 find $MUSIC_PATH -iname "*.flac" >> $PROJECT_PATH/$NAME
+
+
+sed 's/\/media\/brian\/Music 2015 Backup\/music-lib\///' music_cache.txt > mc.tx
+sed s/\\//\|/g mc.txt > mc3.txt
+sed s/' - '/\|/ mc3.txt > mc4.txt
+sed s/\\.[[:space:]]/\|/ mc4.txt > mc5.txt
