c7684d4ad5c5849956b19a32ac5c7173694087ac
Brian K <brian8181@gmail.com>
2021-03-22 20:30:49 -0500

    cache.sh working good

--- cache.sh
+++ cache.sh
@@ -3,34 +3,20 @@
 MUSIC_PATH=$1
 PROJECT_PATH=$2
 NAME=$3
+DELIMITER=| # todo
 
 find "$MUSIC_PATH" -iname "*.mp3" > $PROJECT_PATH/$NAME
 find "$MUSIC_PATH" -iname "*.ogg" >> $PROJECT_PATH/$NAME
 find "$MUSIC_PATH" -iname "*.flac" >> $PROJECT_PATH/$NAME
 
-# trim off root
-sed 's/\/mnt\/music\/music-lib\///' ${PROJECT_PATH}/${NAME} | sed s/\\//\|/g | sed s/' - '/\|/ | sed s/\\.[[:space:]]/\|/ > "${PROJECT_PATH}/L1_${NAME}"
+TIMESTAMP=$(~/script/date.sh)
+TMP_NAME="tmp_${TIMESTAMP}.txt"
+CACHE_NAME="${PROJECT_PATH}/${NAME%.*}_${TIMESTAMP}.txt"
 
-# replace / with |
-#x sed s/\\//\|/g mc.txt > mc3.txt
+# trim off root                                             # replace delimiter   # create date & album columns
+sed 's/\/mnt\/music\/music-lib\///' "${PROJECT_PATH}/${NAME}" | sed s/\\//\|/g | sed s/' - '/\|/ | sed s/\\.[[:space:]]/\|/ > $TMP_NAME
+# add disc column                                           # seperate disc & track                                     
+sed -E "s/(\|[[:digit:]][[:digit:]]\|)/\|\1/" "$TMP_NAME" | sed -E "s/([[:digit:]]+)\\./\1\|/" > "${CACHE_NAME}"
 
-# create date & album columns
-#x sed s/' - '/\|/ mc3.txt > mc4.txt
-
-
-# seperate disc & track
-#x sed s/\\.[[:space:]]/\|/ mc4.txt > mc5.txt
-
-
-# cat mc5.txt | grep album | cut -d\| -f 4 mc5.txt | sed s/\\./,/
-# cut -d\| -f '1 2 3 4 5' mc5.txt
-
-
-# disc ?
-# sed -E "s/(\|[[:digit:]]*\|)/\|\1/" mc5.txt
-
-# disc ?
-# sed -E "s/(\|[[:digit:]][[:digit:]]\|)/\|\1/" mc5.txt > mc6.txt
-
-# track ?
-# sed -E "s/([[:digit:]]*)\\./\1\|/" mc6.txt
+echo ${CACHE_NAME}
+#cat ??????????????_cache.txt   
