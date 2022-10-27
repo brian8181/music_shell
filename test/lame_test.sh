2372cb6f4eaea6799e384d3a45e0b7f95f58fb93
brian8181 <brian8181@gmail.com>
2022-10-27 10:11:54 -0500

    update file naming

--- test/lame_test.sh
+++ test/lame_test.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
-#../lame_recode.sh ./test1.mp3 ./test1.recode_test.mp3
+# clean up first
+rm *.test_run.mp3
 
-#../lame_recode.sh ./test1_no_art.mp3 ./test1_no_art.recode_test2.mp3 
-../lame_recode.sh ./test1_no_art.mp3 ./test1_no_art.recode_test2.mp3 -V3
+# run test or comment out test
+../lame_recode.sh ./test1.mp3 ./test1.test_run.mp3 -V3
+../lame_recode.sh ./test2.mp3 ./test2.test_run.mp3 -V3
+
+../lame_recode.sh ./test1.add_art_from_file.mp3 ./test1.add_art_from_file.test_run.mp3 -V3
+../lame_recode.sh ./test2.add_art_from_file.mp3 ./test2.add_art_from_file.test_run.mp3 -V3
