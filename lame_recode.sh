48965f0cfcc216fb3f12b2f91f9de8feed4862e1
brian8181 <brian8181@gmail.com>
2022-11-01 20:08:39 -0500

    make changes so I could runit

--- lame_recode.sh
+++ lame_recode.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-
-IFILES=$@
 IFS='|'
 
-for $IFILE in $@;
+for IFILE in $@;
 do
     # check if art exist
-    ART=${$IFILE%%/*}/front.jpg
-    if [ -f "$ART" ]; then
-        OPTIONS="$OPTIONS --ti $ART"
-    fi
+    # ART=${$IFILE%%/*}/front.jpg
+    # if [ -f "$ART" ]; then
+    #     OPTIONS="$OPTIONS --ti $ART"
+    # fi
 
     TAGS=$(./tag "$IFILE")
     ARTIST=${TAGS%,*,*,*}
     ALBUM=${TAGS##*,*,}
     TITLE=${TAGS#*,*,*,*} 
 
-    lame -V2 "$IFILE" "${IFILE%*.flac}".mp3
+    # lame -V2 "$IFILE" "${IFILE%*.mp3}".mp3
+    lame -V2 "$IFILE" "${IFILE}.1"
     # lame -V2 --ti "$ART" --tt "$TITLE" --ta "$ARTIST" --tl "$ALBUM"--ty "$YEAR" --tc "$COMMENT" --tn "$TRACK" --tg "$GENRE" "$IFILE" "~/tmp/$IFILE"
 done
