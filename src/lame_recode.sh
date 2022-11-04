4d20d6f4f50c008fc2a4b9c86fb554447dea0caf
brian8181 <brian8181@gmail.com>
2022-11-04 18:43:59 -0500

    fix tags ...

--- src/lame_recode.sh
+++ src/lame_recode.sh
@@ -16,16 +16,22 @@ $
     TAGS=$(./src/tag "$IFILE")
     echo File: "$IFILE"
     echo Tags: "$TAGS"
-    #FIRST={TAGS%%,*}
-    LAST=${TAGS##*,}
-    COMMENT=$LAST
-    REMOVE_LAST=${TAGS%,*}
-    REMOVE_LAST=${REMOVE_LAST%,*}
-    LAST=${TAGS##*,}
-    GENRE=$LAST
 
+    COMMENT=${TAGS##*,}
+    TAGS=${TAGS%,*} # REMOVE_LAST
+    GENRE=${TAGS##*,}
+    TAGS=${TAGS%,*} # REMOVE_LAST
+    TITLE=${TAGS##*,}
+    TAGS=${TAGS%,*} # REMOVE_LAST
+    YEAR=${TAGS##*,}
+    TAGS=${TAGS%,*} # REMOVE_LAST
+    ALBUM=${TAGS##*,}
+    TAGS=${TAGS%,*} # REMOVE_LAST
+    ARTIST=${TAGS##*,}
+    TAGS=${TAGS%,*} # REMOVE_LAST
+    TRACK=${TAGS##*,}
+    echo "OUTPUT: $TRACK | $ARTIST | $ALBUM | $TITLE | $YEAR | $GENRE | $COMMENT"
 
-    echo "OUTPUT: $TRACK | $ARTIST | $ALBUM | $TITLE"
     # lame -V4 "$IFILE" "${IFILE%*.mp3}".mp3
     lame -V4 "$IFILE" "${IFILE}.1"
     # lame -V4 --ti "$ART" --tt "$TITLE" --ta "$ARTIST" --tl "$ALBUM"--ty "$YEAR" --tc "$COMMENT" --tn "$TRACK" --tg "$GENRE" "$IFILE" "~/tmp/$IFILE"
