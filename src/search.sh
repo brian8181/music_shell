37538987b6666f009c34408514c1c4c854419d43
Brian K Preston <brian8181@gmail.com>
2023-03-20 09:28:43 -0500

    modify search.sh ...

--- src/search.sh
+++ src/search.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 cat $2 | egrep --color=always -i "$1"  | tee playing 
-echo -e "Match Count: \e[31m$(cat "$2" | wc -l)\e[0m ... searching for -> \e[31m\"$1\"\e[0m"
+echo -e "Match Count: \e[31m$(cat "playing" | wc -l)/$(cat "$2" | wc -l)\e[0m ... searching for -> \e[31m\"$1\"\e[0m"
