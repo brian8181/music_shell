28a09ff38dbc9fc28afd66c500c8f78c00df0e30
Brian K <brian8181@gmail.com>
2024-02-14 16:09:28 -0600

    add & refactor tools, plus intersect tool ...

---
+++ tools/src/intersect.cpp
@@ -0,0 +1,16 @@
+// License:    None
+// Author:     Brian K Preston
+// File Name:  intersect.hpp
+// Build Date: Wed Feb 14 04:03:20 PM CST 2024
+// Version:    0.0.1
+
+#include "intersect.hpp"
+
+intersect::intersect()
+{
+}
+
+intersect::~intersect()
+{
+
+}
---
+++ tools/src/intersect.hpp
@@ -0,0 +1,21 @@
+// License:    None
+// Author:     Brian K Preston
+// File Name:  intersect.cpp
+// Build Date: Wed Feb 14 04:03:20 PM CST 2024
+// Version:    0.0.1
+
+#ifndef __intersect_HPP
+#define __intersect_HPP
+
+class intersect
+{
+
+public:
+	intersect();
+	virtual ~intersect();
+
+private:
+
+};
+
+#endif
