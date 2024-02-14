d4a91afbf9f9a7808eef1aa5a365d9be4630679f
Brian K <brian8181@gmail.com>
2024-02-13 20:54:49 -0600

    still working on remap prefix ...

--- src/remap.sh
+++ src/remap.sh
@@ -2,16 +2,18 @@ FILE_NAME=$1
 PREFIX=$2
 NEW_PREFIX=$3
 
+#TMP_PREFIX=$(cat $NEW_PREFIX | sed s/\\//x/g)
+TMP_PREFIX=$(echo $NEW_PREFIX | sed "s/F/x/g")
 # replace delimter backslash with forward slash
 EXPR="s/^.*music-lib/'${NEW_PREFIX}'/g"
 TEST=xx
 #FILE_CONTENTS=$(cat $FILE_NAME | sed 's/\\/\//g'  | sed ${EXPR})
 FILE_CONTENTS=$(cat $FILE_NAME | sed 's/\\/\//g'  | sed s/^.*music-lib/${TEST}/g)
 echo "FILE_CONTENT:$FILE_CONTENTS"
-# FILE_CONTENTS=$(cat "$FILE_NAME" | sed "s/^.*music-lib/x/g")
-# echo "OUT:$FILE_CONTENTS"
 
-echo "${EXPR}"
+# testing
+echo "PREFIX=$PREFIX"
+echo "NEW_PREFIX=$NEW_PREFIX"
+echo "TMP_PREFIX=${TMP_PREFIX}"
+echo "EXPR=${EXPR}"
 echo "FILE_NAME_OUT=${FILE%.*}.txt"
-echo "PREFIX:$PREFIX"
-echo "NEW_PREFIX:$NEW_PREFIX"
