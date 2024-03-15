b0f33f1fbadf8a5a2ca393a5517c403d5a4bbf23
Brian K <brian8181@gmail.com>
2024-03-15 08:52:25 -0500

    and_search fix ...

--- src/and_search.sh
+++ src/and_search.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
-# param 1 & parm 2
 
-cat ../cache.txt | egrep --color=always -i "$1" | egrep --color=always -i "$2"
+SEARCH_TERM1=$1
+SEARCH_TERM2=$2
+CACHE_NAME=$3
+
+
+cat "$CACHE_NAME" | egrep --color=always -i "$SEARCH_TERM1" | egrep --color=always -i "$SERACH_TERM2"
 #echo -e "Match Count: \e[31m$(cat "playing" | wc -l)/$(cat "$1" | wc -l)\e[0m ... searching for -> \e[31m\"../cache.txt\"\e[0m"
