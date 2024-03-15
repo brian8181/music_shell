2ed52b3fdaca368582ef3d9edb27dca0b0ae2868
Brian K <brian8181@gmail.com>
2024-03-15 16:43:38 -0500

    add scripts ...

--- src/cache.sh
+++ src/cache.sh
@@ -4,7 +4,7 @@ MUSIC_PATH=$1
 PROJECT_PATH=$2
 NAME=$3
 DELIMITER=| # todo
-# todo
+
 # find .. -name "*.flac" -o -name "*.mp3" -o -name "*.wma"
 find "$MUSIC_PATH" -iname "*.mp3" > $PROJECT_PATH/$NAME
 find "$MUSIC_PATH" -iname "*.ogg" >> $PROJECT_PATH/$NAME
@@ -18,6 +18,4 @@ CACHE_NAME="${PROJECT_PATH}/${NAME%.*}_${TIMESTAMP}.txt"
 sed 's/\/mnt\/music\/music-lib\///' "${PROJECT_PATH}/${NAME}" | sed s/\\//\|/g | sed s/' - '/\|/ | sed s/\\.[[:space:]]/\|/ > $TMP_NAME
 # add disc column                                           # seperate disc & track
 sed -E "s/(\|[[:digit:]][[:digit:]]\|)/\|\1/" "$TMP_NAME" | sed -E "s/([[:digit:]]+)\\./\1\|/" > "${CACHE_NAME}"
-
 echo ${CACHE_NAME}
-#cat ??????????????_cache.txt
