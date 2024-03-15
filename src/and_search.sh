29529afa69fe4fe8e17c63b1f3e4bf98e00ce4d6
Brian K <brian8181@gmail.com>
2024-03-15 16:10:57 -0500

    and_search.sh working ...

--- src/and_search.sh
+++ src/and_search.sh
@@ -4,6 +4,5 @@ SEARCH_TERM1=$1
 SEARCH_TERM2=$2
 CACHE_NAME=$3
 
-cat "$CACHE_NAME" | egrep --color=always -i "$SEARCH_TERM1" | egrep --color=always -i "$SERACH_TERM2" | tee playing
-
-#echo -e "Match Count: \e[31m$(cat playing | wc -l) / $(cat "$CACHE_NAME" | wc -l)\e[0m ... searching for -> \e[31m\""$SEARCH_TERM1 & $SEARCH_TERM2"\"\e[0m"
+cat "$CACHE_NAME" | egrep --color=always -i "$SEARCH_TERM1" | egrep --color=always -i "$SEARCH_TERM2" | tee playing
+echo -e "Match Count: \e[31m$(cat playing | wc -l) / $(cat "$CACHE_NAME" | wc -l)\e[0m ... searching for -> \e[31m\""$SEARCH_TERM1 \& $SEARCH_TERM2"\"\e[0m"
