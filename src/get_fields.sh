634d0f2a5aeb036d8dd068e5a16219f03797c32c
Brian K <brian8181@gmail.com>
2024-03-17 11:53:11 -0500

    added utility folder ...

--- src/get_fields.sh
+++ src/get_fields.sh
@@ -1,11 +1,5 @@
 #! /usr/bin/bash
 
-export folder=1
-export artist=2
-export date=3
-export album=4
-export disc=5
-export track=6
-export title=7
+./fields.sh
 
 sed -E "s/^(.+?)\|(.+?)\|(.+?)\|(.+?)\|(.+?)\|(.+?)\|(.+?)$/\\$folder~~\\$artist~~\\$date~~\\$album~~\\$disc~~\\$track~~\\$title~~/" ../test/cache.txt
