80cc3c58deaf2191e7e6bee3192ccb0e5dd76550
Brian K <brian8181@gmail.com>
2024-03-18 09:07:18 -0500

    search history ...

--- src/search.sh
+++ src/search.sh
@@ -3,7 +3,7 @@
 SEARCH_TERM=$1
 CACHE_NAME=$2
 
-echo $SEARCH_TERM >> cache/search_history
+echo $SEARCH_TERM >> cache/search_history_all
 
 cat "$CACHE_NAME" | egrep --color=always -i "$SEARCH_TERM" | tee playing
 echo -e "Match Count: \e[31m$(cat playing | wc -l) / $(cat "$CACHE_NAME" | wc -l)\e[0m ... searching for -> \e[31m\""$SEARCH_TERM"\"\e[0m"
---
+++ src/show_search_history.sh
@@ -0,0 +1,1 @@
+tail -n5 cache/search_history_all
