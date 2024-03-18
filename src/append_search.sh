a0fdffd0481ca9103228d26fd549df1c8b456da0
Brian K <brian8181@gmail.com>
2024-03-18 08:34:21 -0500

    append, search_histroy ...

--- src/append_search.sh
+++ src/append_search.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 SEARCH_TERM=$1
 CACHE_NAME=$2
 
-echo $SEARCH_TERM >> cache/search_history
+$SEARCH_TERM >> cache/search_history
 
-cat "$CACHE_NAME" | egrep --color=always -i "$SEARCH_TERM" | tee playing
+cat "$CACHE_NAME" | egrep --color=always -i "$SEARCH_TERM" | tee --append playing
 echo -e "Match Count: \e[31m$(cat playing | wc -l) / $(cat "$CACHE_NAME" | wc -l)\e[0m ... searching for -> \e[31m\""$SEARCH_TERM"\"\e[0m"
--- src/utility/vars.sh
+++ src/utility/vars.sh
@@ -5,3 +5,4 @@ export CURRENT_SONG=$2
 export CURRENT_POS=$3
 export CONTINOUS_PLAY=$4
 export RANMDON_PLAY=$5
+export NOW_PLAYING_LEN=$(cat playing | wc -l)
