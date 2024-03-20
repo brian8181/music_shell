ef195676f55d9aff49d7a22c54577ac840eefadf
Brian K <brian8181@gmail.com>
2024-03-20 15:39:22 -0500

    commit all ...

--- src/utility/vars.sh
+++ src/utility/vars.sh
@@ -5,4 +5,5 @@ export CURRENT_SONG=$2
 export CURRENT_POS=$3
 export CONTINOUS_PLAY=$4
 export RANMDON_PLAY=$5
-export NOW_PLAYING_LEN=$(cat playing | wc -l)
+export HISTORY_LEN=100
+export NOW_PLAYING_LEN=$(cat ../cache/playing | wc -l)
