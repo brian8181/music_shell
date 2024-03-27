dbc5198d22949c9e22b2f27db16f98ae8db43b0a
Brian K <brian8181@gmail.com>
2024-03-27 03:49:17 -0500

    select.debug partially working ...

--- src/select.debug.sh
+++ src/select.debug.sh
@@ -43,8 +43,8 @@ VALID_CHARS="[-a-zA-Z0-9_ ]"
 RECORD="^\(${VALID_CHARS}*\)\|\([0-9][0-9][0-9][0-9]\)\|\(${VALID_CHARS}*\)\|\([0-9][0-9]\)?\|\([0-9][0-9]\)?\|\(${VALID_CHARS}*\)\.\(mp3|ogg|flac\)$"
 RECORD="^\(${VALID_CHARS}*\|\)\\([0-9][0-9][0-9][0-9]\\|\\)\\(${VALID_CHARS}*\\|\\)\\([0-9][0-9]?\\|\\)\\([0-9][0-9]?\\|\\)\\(${VALID_CHARS}*\\)\\.(mp3|ogg|flac)$"
 #./show_all.sh | sed "s/^\\(${VALID_CHARS}*\\)\\|\\([0-9][0-9][0-9][0-9]\\)/{\2\1}/g"
-cut -d"|" -f "$ARTIST $DATE $ALBUM $DISC" cache/cache.txt > s.txt
-cat s.txt | sed "s/^\([a-zA-Z ]*\)|\([0-9][0-9][0-9][0-9]\)|\([a-zA-Z ]*\)|/\2\3\1/g"
+cut -d"|" -f "$ARTIST $DATE $ALBUM $DISC $TRACK" cache/cache.txt > s.txt
+cat s.txt | sed "s/^\([a-zA-Z ]*\)|\([0-9][0-9][0-9][0-9]\)|\([a-zA-Z ]*\)|\(\)|\([0-9][0-9]\)/\5-\2\1\3/g"
 
 # what the fuck can't seem to get this to match as expected (match the artist) ...
 #./show_all.sh | cut -d "|" -f "1 2 5" | sed -E s/[a-zA-Z]*/xxx/g
