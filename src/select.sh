d5a22e4255828afae9682840b7ed206266567f70
Brian K <brian8181@gmail.com>
2024-03-27 22:00:45 -0500

    working on select its comming along, sorta ...

--- src/select.sh
+++ src/select.sh
@@ -21,3 +21,4 @@ cut -d"|" -f "1 2 3 4 5 6 7" cache/cache.txt > s.txt
 #  ^(1)FOLDER              |(2)ARTIST               |(3)DATE          |(4)ALBUM       |(5)Disc           |(6)TRACK         |(6)TITLE                .(7)EXT$
 R="([[:alnum:]]+)\|([[:alnum:][:blank:]]*)\|([[:digit:]]{4})\|([[:alnum:][:blank:]]*)\|([[:digit:]]{2}?)\|([[:digit:]]{2})\|([[:alnum:][:blank:]]*)\.(mp3|ogg|flac)$"
 cat cache/cache.txt | sed -E "s/$R/\8\7\6\5\4\3\2\1/g"
+#cat cache/cache.txt | sed -E "s/$R/$FIELDS/g"
