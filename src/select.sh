ddf9cf0b4bfec88b79a70210789e7b4eaeda86fe
Brian K <brian8181@gmail.com>
2024-03-27 22:44:16 -0500

    WORKING EXCEPT FOR CMD LINE ARG! ....

--- src/select.sh
+++ src/select.sh
@@ -16,7 +16,7 @@ VALID_CHARS="[-a-zA-Z0-9_ ]"
 cut -d"|" -f "$ARTIST $DATE $ALBUM $TRACK $TITLE" cache/cache.txt > s.txt
 
 #  ^(1)FOLDER       |(2)ARTIST     |(3)DATE     |(4)ALBUM      |(5)Disc     |(6)TRACK     |(7)TITLE      .(8)EXT$
-REGX="${FOLDER_EXP}\|${STRING_EXP}\|${DATE_EXP}\|${STRING_EXP}\|${DISC_EXP}\|${TRACK_EXP}\|${STRING_EXP}\.${EXT_EXP}"
+REGX="^${FOLDER_EXP}\|${STRING_EXP}\|${DATE_EXP}\|${STRING_EXP}\|${DISC_EXP}\|${TRACK_EXP}\|${STRING_EXP}\.${EXT_EXP}$"
 FOLDER_EXP="([[:alnum:]]+)"
 STRING_EXP="([[:alnum:][:blank:]]*)"
 DATE_EXP="([[:digit:]]{4})"
@@ -25,5 +25,5 @@ TRACK_EXP="([[:digit:]]{2})"
 EXT_EXP="(mp3|ogg|flac)"
 
 # PARAMS="s/${REGX}/\\${EXT} \\${ALBUM} \\${ARTIST} \\${DATE} \\${ALBUM} \\${TRACK} \\${TITLE}/g"
-# cat cache/cache.txt | sed -E "$PARAMS"
-cat cache/cache.txt | sed -E "s/([[:alnum:]]+)\|([[:alnum:][:blank:]]*)\|([[:digit:]]{4})\|([[:alnum:][:blank:]]*)\|([[:digit:]]{2}?)\|([[:digit:]]{2})\|([[:alnum:][:blank:]]*)\.(mp3|ogg|flac)$/\\${EXT} \\${ALBUM} \\${ARTIST} \\${DATE} \\${ALBUM} \\${TRACK} \\${TITLE}/g"
+# cat cache/cache.txt | sed -E "$PARAMS" cache/cxche.txt
+sed -E "s/([[:alnum:]]+)\|([[:alnum:][:blank:]]*)\|([[:digit:]]{4})\|([[:alnum:][:blank:]]*)\|([[:digit:]]{2}?)\|([[:digit:]]{2})\|([[:alnum:][:blank:]]*)\.(mp3|ogg|flac)$/\\${EXT} \\${ALBUM} \\${ARTIST} \\${DATE} \\${ALBUM} \\${TRACK} \\${TITLE}/g" cache/cache.txt
