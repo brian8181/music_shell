e6c3d4e3e77bd2ddb5d8d48a1f34036da5fd605c
Brian K <brian8181@gmail.com>
2024-03-27 22:04:22 -0500

    WORKING EXCEPT FOR CMD LINE ARG!

--- src/select.sh
+++ src/select.sh
@@ -15,10 +15,9 @@ TITLE=7
 EXT=8
 
 VALID_CHARS="[-a-zA-Z0-9_ ]"
-#cut -d"|" -f "$ARTIST $DATE $ALBUM $TRACK $TITLE" cache/cache.txt > s.txt
-cut -d"|" -f "1 2 3 4 5 6 7" cache/cache.txt > s.txt
+cut -d"|" -f "$ARTIST $DATE $ALBUM $TRACK $TITLE" cache/cache.txt > s.txt
 
 #  ^(1)FOLDER              |(2)ARTIST               |(3)DATE          |(4)ALBUM       |(5)Disc           |(6)TRACK         |(6)TITLE                .(7)EXT$
 R="([[:alnum:]]+)\|([[:alnum:][:blank:]]*)\|([[:digit:]]{4})\|([[:alnum:][:blank:]]*)\|([[:digit:]]{2}?)\|([[:digit:]]{2})\|([[:alnum:][:blank:]]*)\.(mp3|ogg|flac)$"
-cat cache/cache.txt | sed -E "s/$R/\8\7\6\5\4\3\2\1/g"
+cat cache/cache.txt | sed -E "s/$R/\\$EXT \\$ALBUM  \\$ARTIST \\$DATE \\$ALBUM \\$TRACK \\$TITLE/g"
 #cat cache/cache.txt | sed -E "s/$R/$FIELDS/g"
