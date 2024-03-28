449994c309a77bd4b525668e40114cd320cf62aa
Brian K <brian8181@gmail.com>
2024-03-27 22:18:57 -0500

    WORKING EXCEPT FOR CMD LINE ARG!

--- src/select.sh
+++ src/select.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#$~/src/music_shell/src/utility/fields.sh
+~/src/music_shell/src/utility/fields.sh
 
 FIELDS=$1
-echo ${FIELDS}
-#FIELDS=$(echo $FIELDS | sed -E "s/([[:digit:]]*)/\\\\1/g")
 FOLDER=1
 ARTIST=2
 DATE=3
@@ -18,6 +16,13 @@ VALID_CHARS="[-a-zA-Z0-9_ ]"
 cut -d"|" -f "$ARTIST $DATE $ALBUM $TRACK $TITLE" cache/cache.txt > s.txt
 
 #  ^(1)FOLDER              |(2)ARTIST               |(3)DATE          |(4)ALBUM       |(5)Disc           |(6)TRACK         |(6)TITLE                .(7)EXT$
-R="([[:alnum:]]+)\|([[:alnum:][:blank:]]*)\|([[:digit:]]{4})\|([[:alnum:][:blank:]]*)\|([[:digit:]]{2}?)\|([[:digit:]]{2})\|([[:alnum:][:blank:]]*)\.(mp3|ogg|flac)$"
-cat cache/cache.txt | sed -E "s/$R/\\$EXT \\$ALBUM  \\$ARTIST \\$DATE \\$ALBUM \\$TRACK \\$TITLE/g"
-#cat cache/cache.txt | sed -E "s/$R/$FIELDS/g"
+REGX="^${FOLDER_EXP}\|${STRING_EXP}\|${DATE_EXP}\|${STRING_EXP}\|${DISC_EXP}\|${TRACK_EXP}\|${STRING_EXP}\.${EXT_EXP}$"
+
+FOLDER_EXP="([[:alnum:]]+)"
+STRING_EXP="([[:alnum:][:blank:]]*)"
+DATE_EXP="([[:digit:]]{4})"
+DISC_EXP="([[:digit:]]{2}?)"
+TRACK_EXP="([[:digit:]]{2})"
+EXT_EXP="(mp3|ogg|flac)"
+
+cat cache/cache.txt | sed -E "s/([[:alnum:]]+)\|([[:alnum:][:blank:]]*)\|([[:digit:]]{4})\|([[:alnum:][:blank:]]*)\|([[:digit:]]{2}?)\|([[:digit:]]{2})\|([[:alnum:][:blank:]]*)\.(mp3|ogg|flac)$/\\$EXT \\$ALBUM  \\$ARTIST \\$DATE \\$ALBUM \\$TRACK \\$TITLE/g"
