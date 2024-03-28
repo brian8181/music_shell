807299671fa34e091c5905bbfb93960037320158
Brian K <brian8181@gmail.com>
2024-03-27 21:59:54 -0500

    working on select its comming along, sorta ...

--- src/select.sh
+++ src/select.sh
@@ -18,6 +18,6 @@ VALID_CHARS="[-a-zA-Z0-9_ ]"
 #cut -d"|" -f "$ARTIST $DATE $ALBUM $TRACK $TITLE" cache/cache.txt > s.txt
 cut -d"|" -f "1 2 3 4 5 6 7" cache/cache.txt > s.txt
 
-#  ^(1)FOLDER              |(2)ARTIST               |(3)DATE          |(4)ALBUM                |(7)Disc         |(5)TRACK         |(6)TITLE                .(7)EXT$
-R="([[:alnum:][:blank:]]+)\|([[:alnum:][:blank:]]+)\|([[:digit:]]{4})\|([[:alnum:][:blank:]]+)\|([[:digit:]]{0,2})|([[:digit:]]{2})\|([[:alnum:][:blank:]]+)\.(mp3|ogg|flac)"
+#  ^(1)FOLDER              |(2)ARTIST               |(3)DATE          |(4)ALBUM       |(5)Disc           |(6)TRACK         |(6)TITLE                .(7)EXT$
+R="([[:alnum:]]+)\|([[:alnum:][:blank:]]*)\|([[:digit:]]{4})\|([[:alnum:][:blank:]]*)\|([[:digit:]]{2}?)\|([[:digit:]]{2})\|([[:alnum:][:blank:]]*)\.(mp3|ogg|flac)$"
 cat cache/cache.txt | sed -E "s/$R/\8\7\6\5\4\3\2\1/g"
