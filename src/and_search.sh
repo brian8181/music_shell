730c73d33e243b58ee85434a9c620e0d141412fe
Brian K <brian8181@gmail.com>
2024-03-31 10:46:42 -0500

    create show_queue, show_list ...

--- src/and_search.sh
+++ src/and_search.sh
@@ -5,6 +5,6 @@ SEARCH_TERM2=$2
 CACHE_NAME=$3
 IGNORE_CASE=$4
 
-cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM1" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM2" | tee playing
+cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM1" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM2" | tee queue
 cp playing cache/hist_"$date $SEARCH_TERM1_&&_$SEARCH_TERM2".txt
 #echo -e "Match Count: \e[31m$(cat playing | wc -l) / $(cat "$CACHE_NAME" | wc -l)\e[0m ... searching for -> \e[31m\""$SEARCH_TERM1 \& $SEARCH_TERM2"\"\e[0m"
--- src/search.sh
+++ src/search.sh
@@ -5,5 +5,5 @@ CACHE_NAME=$2
 IGNORE_CASE=$3
 
 echo "$SEARCH_TERM $IGNORE_CASE" >> cache/search_history_all
-cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM" | tee playing
+cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM" | tee queue
 cp playing cache/hist_"$(date.sh) \"$SEARCH_TERM\"".txt
