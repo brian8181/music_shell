3e8317cdd1aa06eb1d6afafee1462b2f252613dd
Brian K <brian8181@gmail.com>
2024-04-03 17:27:06 -0500

    update or search with settings.sh ...

--- src/or_search.sh
+++ src/or_search.sh
@@ -8,6 +8,6 @@ SEARCH_TERM2=$2
 CACHE_NAME="$HOME/.music_shell/cache/$3"
 IGNORE_CASE=$4
 
-echo "$SEARCH_TERM1 <OR> $SEARCH_TERM2 $IGNORE_CASE" >> /home/brian/.music_shell/cache/search_history_all
-cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM1" | tee /home/brian/.music_shell/queue
-cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM2" | sort -u | tee -a /home/brian/.music_shell/queue
+echo "$SEARCH_TERM1 <OR> $SEARCH_TERM2 $IGNORE_CASE" >> $HOME/.music_shell/cache/search_history_all
+cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM1" | tee $HOME/.music_shell/queue
+cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM2" | sort -u | tee -a $HOME/.music_shell/queue
