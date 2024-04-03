8a188f9ec3b7e2aae3c075a44a73a98f6dda6156
Brian K <brian8181@gmail.com>
2024-04-03 17:21:21 -0500

    update and search with settings.sh ...

--- src/or_search.sh
+++ src/or_search.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
+HOME='/home/brian'
 SEARCH_TERM1=$1
 SEARCH_TERM2=$2
 CACHE_NAME="/home/brian/.music_shell/cache/$3"
 IGNORE_CASE=$4
 
 echo "$SEARCH_TERM1 <OR> $SEARCH_TERM2 $IGNORE_CASE" >> /home/brian/.music_shell/cache/search_history_all
-cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM1" | tee ~/.music_shell/queue
-cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM2" | tee -a ~/.music_shell/queue
-
-# S1=$(cat "$CACHE_NAME" | egrep --color=always -i "$SEARCH_TERM1")
-# S2=$(cat "$CACHE_NAME" | egrep --color=always -i "$SEARCH_TERM2")
-
-# echo $S1 $S2 | tee playing
-#cp playing cache/hist_"$date $SEARCH_TERM1_||_$SEARCH_TERM2".txt
-#echo -e "Match Count: \e[31m$(cat playing | wc -l) / $(cat "$CACHE_NAME" | wc -l)\e[0m ... searching for -> \e[31m\""$SEARCH_TERM1 \| $SEARCH_TERM2"\"\e[0m"
+cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM1" | tee /home/brian/.music_shell/queue
+cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM2" | sort -u | tee -a /home/brian/.music_shell/queue
