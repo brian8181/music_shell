a9d023bcc4338ddc3afc8f0c1b34a8799ac42040
Brian K <brian8181@gmail.com>
2024-04-03 18:30:01 -0500

    update for settings.sh ...

--- src/search.sh
+++ src/search.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# load export settings
+./settings.sh
+
 SEARCH_TERM=$1
-CACHE_NAME="/home/brian/.music_shell/cache/$2"
+CACHE_NAME="$HOME/.music_shell/cache/$2"
 IGNORE_CASE=$3
 
-echo "$SEARCH_TERM $IGNORE_CASE" >> /home/brian/.music_shell/cache/search_history_all
-cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM" | tee ~/.music_shell/queue
+echo "$SEARCH_TERM $IGNORE_CASE" >> $HOME/.music_shell/cache/search_history_all
+cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM" | tee $HOME/.music_shell/queue
 #cp music_shell/queue music_shell/cache/hist_"$(date.sh) \"$SEARCH_TERM\"".txt
