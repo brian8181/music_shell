1da861c9dbe61ef55dafafa8de43d4adf89e155d
Brian K <brian8181@gmail.com>
2024-04-03 18:33:29 -0500

    add fields to settings.sh

--- src/settings.sh
+++ src/settings.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 export HOME='/home/brian'
+
+# export CURRENT_PLAYLIST=$1
+# export CURRENT_SONG=$2
+# export CURRENT_POS=$3
+# export CONTINOUS_PLAY=$4
+# export RANMDON_PLAY=$5
+# export HISTORY_LEN=100
+#export NOW_PLAYING_LEN=$(cat ../cache/queue | wc -l)
+
+export FOLDER='1'
+export ARTIST='2'
+export DATE='3'
+export ALBUM='4'
+export DISC='5'
+export TRACK='6'
+export TITLE='7'
