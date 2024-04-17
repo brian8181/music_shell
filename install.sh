bc5d3ad269b8f9f2d160422fbaa3a8a86fc38a3c
Brian K <brian8181@gmail.com>
2024-04-17 12:59:09 -0500

    add checks to install.sh ...

--- install.sh
+++ install.sh
@@ -2,14 +2,15 @@
 
 if [ -e "~/.music_shell/cache" ]
     md -p ~/.music_shell/cache
+    touch ~/.music_shell/queue
+    touch ~/.music_shell/config.txt
 fi
 
 if [ -e "~/bin" ]; then
     md ~/bin
+    cp -rf ../src/* ~/bin
 fi
 
-cp -rf ../src/* ~/bin
-
-ln ./def_search.sh s
-touch ~/.music_shell/queue
-touch ~/.music_shell/config.txt
+if [ -e "./def_search.sh" ]
+    ln ./def_search.sh s
+fi
