20457b52f5237dacd9eef45ac1de2f8f5d5f2d37
Brian K <brian8181@gmail.com>
2024-04-20 18:52:21 -0500

    testing create_playlist ...

---
+++ src/create_playlist.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# load export settings
+./settings.sh
+
+OPTIONS=NULL
+SEARCH_TERM=$1
+CACHE_NAME="$HOME/.music_shell/cache/$2"
+PLAYLIST_NAME=$3
+IGNORE_CASE=$4
+
+if [[ ! -r "$CACHE_NAME" ]]; then
+    echo "error: $CACHE_NAME does not exists"
+    exit
+fi
+
+if [ -z "$SEARCH_TERM" ]; then
+    echo "Error: no "SEARCH_TERM" specified ..."
+    exit
+fi
+
+cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM" | tee -a $HOME/"${PLAYLIST_NAME}.m3u.swp"
+cat $HOME/"${PLAYLIST_NAME}.m3u.swp" | sort -u > $HOME/"${PLAYLIST_NAME}.m3u"
+#rm $HOME/"${PLAYLIST_NAME}.m3u.swp"
