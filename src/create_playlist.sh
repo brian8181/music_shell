a673c6c8a5f5a3ed1b67df70308ae0efb4a60326
Brian K <brian8181@gmail.com>
2024-04-27 09:41:38 -0500

    update creat_playliste ...

--- src/create_playlist.sh
+++ src/create_playlist.sh
@@ -44,6 +44,7 @@ PRINT_INFO "\"$FILE\" -> Running... @ $DATE"
 #./settings.sh
 
 #OPTIONS=NULL
+HOME="/home/brian"
 SEARCH_TERM=$1
 CACHE_NAME="$HOME/.music_shell/cache/$2"
 PLAYLIST_NAME=$3
@@ -55,8 +56,8 @@ IGNORE_CASE=$3
 # fi
 
 #
-cat "$CACHE_NAME" | egrep --color=always "$SEARCH_TERM" "$IGNORE_CASE" >> "$HOME/${PLAYLIST_NAME}.m3u.swp"
-#cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM" | tee -a $HOME/"${PLAYLIST_NAME}.m3u.swp"
+cat "$CACHE_NAME" | egrep "$SEARCH_TERM" >> "$HOME/${PLAYLIST_NAME}.m3u.swp"
+#cat "$CACHE_NAME" | egrep --color=always $IGNORE_CASE "$SEARCH_TERM" | tee -a "$HOME/${PLAYLIST_NAME}.m3u.swp"
 cat "$HOME/${PLAYLIST_NAME}.m3u.swp" | sort -u > "$HOME/${PLAYLIST_NAME}.m3u"
 rm $HOME/"${PLAYLIST_NAME}.m3u.swp"
 
