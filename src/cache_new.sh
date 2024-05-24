58b7497421cb4148f5669dfd9987d4d45b407a2f
Brian K <brian8181@gmail.com>
2024-05-24 08:03:03 -0500

    cache_new.sh working!! ...

--- src/cache_new.sh
+++ src/cache_new.sh
@@ -3,17 +3,15 @@
 HOME="/home/brian"
 STORE_PREFIX="/mnt/music/music-lib"
 CONFIG_PREFIX="$HOME/.music_shell"
-CACHE_NAME="cache.m3u"
-QUEUE_NAME="queue.m3u"
 TIME_STAMP="$(date.sh)"
-TMP_NAME="tmp_${TIMESTAMP}.txt"
-#CACHE_NAME="${DEST_PATH}/${NAME%.*}_${TIMESTAMP}.txt"
+CACHE_PATH="${CONFIG_PREFIX}/${TIME_STAMP}_cache.m3u"
+QUEUE_NAME="queue.m3u"
 
-echo "searching \"${STORE_PREFIX}\", wrting cache too \"${CONFIG_PREFIX}/${TIME_STAMP}_${CACHE_NAME}\" ..."
-find "${STORE_PREFIX}" -iregex '^.*\.\(\(mp3\)\|\(flac\)\|\(ogg\)|\(wma\)\)$' > "${CONFIG_PREFIX}/${TIME_STAMP}_${CACHE_NAME}"
-echo "finished writing cache too \"${CONFIG_PREFIX}/${TIME_STAMP}_${CACHE_NAME}\" ..."
+echo "searching \"${STORE_PREFIX}\", writing cache too \"${CACHE_PATH}\" ..."
+find "${STORE_PREFIX}" -iregex '^.*\.\(\(mp3\)\|\(flac\)\|\(ogg\)|\(wma\)\)$' > "${CACHE_PATH}"
+echo "finished writing cache too \"${CACHE_PATH}\" ..."
 
 # trim off root                                             # replace delimiter   # create date & album columns
-# sed 's/\/mnt\/music\/music-lib\///' "${CACHE_NAME}" | sed s/\\//\|/g | sed s/' - '/\|/ | sed s/\\.[[:space:]]/\|/ > $TMP_NAME
+sed 's/\/mnt\/music\/music-lib\///' "${CACHE_PATH}" | sed s/\\//\|/g | sed s/' - '/\|/ | sed s/\\.[[:space:]]/\|/ > "${CACHE_PATH}.tmp"
 # add disc column                                           # seperate disc & track
-# sed -E "s/(\|[[:digit:]][[:digit:]]\|)/\|\1/" "$TMP_NAME" | sed -E "s/([[:digit:]]+)\\./\1\|/" > "${CACHE_NAME}"
+sed -E "s/(\|[[:digit:]][[:digit:]]\|)/\|\1/" "${CACHE_PATH}.tmp" | sed -E "s/([[:digit:]]+)\\./\1\|/" > "${CACHE_PATH}"
