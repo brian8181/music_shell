258c344041e7ea1f4939aa434038cae612a90abf
Brian K <brian8181@gmail.com>
2024-07-13 08:30:51 -0500

    added set_index & state.sh ...

---
+++ src/utility/set_index.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# FILE 'src/set_index.sh'
+# VERSION '0.0.1'
+# FILE_DATE 'Fri Jul 12 07:03:30 AM CDT 2024'
+
+INDEX=$1
+
+CONFIG_PREFIX="$HOME/.music_shell"
+FILE="$CONFIG_PREFIX/index"
+
+echo -e "${FILE}"
+echo -e "${INDEX}\\n" > "${FILE}"
