c799b44ac68e87ecd7778cf04aa8c4c0d7acb418
Brian K Preston <brian8181@gmail.com>
2024-09-14 12:38:06 -0500

    update ...

--- src/to_xml.sh
+++ src/to_xml.sh
@@ -1,1 +1,4 @@
-cat cache.tmp | sed -E 's/^([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|<loc>\1<\/loc>\<artist>\2<artist><date>\3<\/date><album>\4<\/album><disc>\5<\/disc><track>\6<\/track><title>\7\<\/title><ext>\8\<\/ext>/g'
+#!/bin/bash
+
+LIST=$1
+echo $1 | sed -E 's/^([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|([[:alnum:] ]*)\|<loc>\1<\/loc>\<artist>\2<artist><date>\3<\/date><album>\4<\/album><disc>\5<\/disc><track>\6<\/track><title>\7\<\/title><ext>\8\<\/ext>/g'
