8f5c854e0d3d5cc547859b801785ae7c87c6c0d0
Brian K Preston <brian8181@gmail.com>
2024-09-22 18:01:21 -0500

    csv_to_xml works ...

--- src/csv_to_xml.sh
+++ src/csv_to_xml.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 SRC_FILE="$1"
-DST_FILE="$2"
-DST_FILE="${$DST_FILE:-SRC_FILE%*.csv}.xml"
+DST_FILE="${SRC_FILE%*.csv}.xml"
 FORMAT="location,artist,album,title"
 
-if [[! -f "$SRC_FILE" ]]; then
+if [[ !(-f "$SRC_FILE") ]]; then
     echo "Error Not File Found: source file "$SRC_FILE" found ..."
     exit 1
 fi
@@ -19,9 +18,9 @@ LEN_ORG=$(cat "$SRC_FILE" | wc -l)
 LEN_CUR=$(cat "$SRC_FILE" | grep -E '^\".*\",\".*\",\".*\",\".*\"$' | wc -l)
 
 if [[ $LEN_ORG != $LEN_CUR ]]; then
-    echo "Error File Format: source file "$SRC_FILE" wrong format... \n\t"$FORMAT"
+    echo "Error File Format: source file \"$SRC_FILE\" wrong format... \n\t\"$FORMAT\""
     exit 1
 fi
 
 #<?xml version="1.0"?>
-cat "$SRC_FILE" | sed -E 's/\"([^,]*)\"/<~~~~\1~~~~>/g' | sed -E 's/<~~~~(.*)~~~~>,<~~~~(.*)~~~~>,<~~~~(.*)~~~~>,<~~~~(.*)~~~~>/<song>\n\t\t<location>\1<\/location>\n\t\t<artist>\2<\/artist>\n\t\t<album>\3<\/album>\n\t\t<title>\4<\/title>\n\t<\/song>\n/g' > "$DST_FILE"
+cat "$SRC_FILE" | sed -E 's/\"([^,]*)\"/<~~~~\1~~~~>/g' | sed -E 's/<~~~~(.*)~~~~>,<~~~~(.*)~~~~>,<~~~~(.*)~~~~>,<~~~~(.*)~~~~>/<song>\n\t<location>\1<\/location>\n\t<artist>\2<\/artist>\n\t<album>\3<\/album>\n\t<title>\4<\/title>\n<\/song>/g' > "$DST_FILE"
