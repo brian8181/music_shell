98f880ae883cfc6d0d93fcaea7145abda0dd6e55
Brian K Preston <brian8181@gmail.com>
2024-09-23 00:56:05 -0500

    add back error check for destination ...

--- src/csv_to_xml.sh
+++ src/csv_to_xml.sh
@@ -5,14 +5,17 @@ DST_FILE="${SRC_FILE%*.csv}.xml"
 FORMAT="location,artist,album,title"
 
 if [[ !(-f "$SRC_FILE") ]]; then
-    echo "Error Not File Found: source file "$SRC_FILE" found ..."
+    echo "Error Not File Found: source file "\"$SRC_FILE\"" found ..."
     exit 1
 fi
 
-# if [[ -e "$DST_FILE" ]]; then
-#     DST_FILE="${DST_FILE}"
-# fi
 
+if [[ -e "$DST_FILE" ]]; then
+     echo "Error: destination "\"$DST_FILE\"" already exist ..."
+     exit 1
+fi
+
+# todo
 # SRC_FILE_TMP=TMP.csv
 # tail -n $(($(cat $SRC_FILE | wc -l)-1)) > $SRC_FILE_TMP
 
@@ -28,8 +31,10 @@ fi
 echo '<?xml version="1.0"?>' > "$DST_FILE"
 echo "<songs>" >> "$DST_FILE"
 
+# todo
 # DELIMITERS='<~~~~\1~~~~>'
 # INSERT_DELIMTERS_PATTERN='([^,]*)'
+
 FIND_PATTERN='<~~~~(.*)~~~~>,<~~~~(.*)~~~~>,<~~~~(.*)~~~~>,<~~~~(.*)~~~~>'
 REPLACE_PATTERN='<song>\n\t<location>\1<\/location>\n\t<artist>\2<\/artist>\n\t<album>\3<\/album>\n\t<title>\4<\/title>\n<\/song>'
 cat $SRC_FILE | sed -E 's/\"([^,]*)\"/<~~~~\1~~~~>/g' | sed -E "s/${FIND_PATTERN}/${REPLACE_PATTERN}/g" >> "$DST_FILE"
