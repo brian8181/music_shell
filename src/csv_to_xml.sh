939ccfb53f8ea78a1c713990656e0fbc16e9f1b7
Brian K Preston <brian8181@gmail.com>
2024-09-24 11:57:55 -0500

    cvs_to_xml remove header row ...

--- src/csv_to_xml.sh
+++ src/csv_to_xml.sh
@@ -16,28 +16,40 @@ if [[ -e "$DST_FILE" ]]; then
      exit 1
 fi
 
-# todo
-# SRC_FILE_TMP=TMP.csv
-# tail -n $(($(cat $SRC_FILE | wc -l)-1)) > $SRC_FILE_TMP
+# remove header line
+SRC_FILE_TMP="${SRC_FILE%*.csv}.tmp"
+cat "$SRC_FILE" > "$SRC_FILE_TMP"
+sed -i 1d "$SRC_FILE_TMP"
+
+# remove header line
+# SRC_FILE_TMP="${SRC_FILE%*.csv}.tmp"
+# LEN=$(cat "$SRC_FILE" | wc -l)
+# cat  "$SRC_FILE" | tail -n $((LEN-1)) > "$SRC_FILE_TMP"
+
+# get header from src, use to create xml elements
+# HEADERS=$(head -n 1 "$SRC_FILE")
+# HEADER_PATTERN='^(.*),(.*),(.*),(.*)$'
+# echo $HEADERS | sed -E "s/$HEADER_PATTERN/\4 \3 \2 \1/g"
 
 PATTERN='^\"(.*)\",\"(.*)\",\"(.*)\",\"(.*)\"$'
 REPLACE_PATTERN='\t<song>\n\t\t<location>\1<\/location>\n\t\t<artist>\2<\/artist>\n\t\t<album>\3<\/album>\n\t\t<title>\4<\/title>\n\t<\/song>'
 
 # check format
-LEN_ORG=$(cat "$SRC_FILE" | wc -l)
-LEN_CUR=$(cat "$SRC_FILE" | grep -E "$PATTERN" | wc -l)
+LEN_ORG=$(cat "$SRC_FILE_TMP" | wc -l)
+LEN_CUR=$(cat "$SRC_FILE_TMP" | grep -E "$PATTERN" | wc -l)
 
-if [[ $LEN_ORG != $LEN_CUR ]]; then
-    echo "Error File Format: source file \"$SRC_FILE\" wrong format... \n\t\"$FORMAT\""
-    exit 1
-fi
+# if [[ $LEN_ORG != $LEN_CUR ]]; then
+#     echo -e "Error File Format: source file \"$SRC_FILE\" wrong format... \n\t\"$FORMAT\""
+#     exit 1
+# fi
 
 # xml header
 echo '<?xml version="1.0"?>' > "$DST_FILE"
 echo "<!-- FILE_DATE: ${COMMENT:=${CURRENT_DATETIME}} -->" >> "$DST_FILE"
 echo -e "<songs>" >> "$DST_FILE"
 
-cat $SRC_FILE | sed -E "s/${PATTERN}/${REPLACE_PATTERN}/g" >> "$DST_FILE"
+cat "$SRC_FILE_TMP" | sed -E "s/${PATTERN}/${REPLACE_PATTERN}/g" >> "$DST_FILE"
+#rm "$SRC_FILE_TMP"
 
 # close element
 echo -e "</songs>" >> "$DST_FILE"
