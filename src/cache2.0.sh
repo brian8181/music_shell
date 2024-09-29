5df90b91161ed4d76b7167ef9e375a9fe45b8053
Brian K Preston <brian8181@gmail.com>
2024-09-29 07:00:00 -0500

    debugging cache2 singles, hjalf way working ...

--- src/cache2.0.sh
+++ src/cache2.0.sh
@@ -14,10 +14,7 @@ echo "searching \"${STORE_PREFIX}\", writing cache too \"${CACHE}\" ..."
 
 #find "${STORE_PREFIX}" -iregex '^.*\.\(\(mp3\)\|\(flac\)\|\(ogg\)|\(m4a\)|\(wma\)\)$' > "${CACHE}" # all
 
-TMP_CACHE="{CACHE%*.m3u}.txt"
-find "${STORE_PREFIX}" -iregex '^.*\.\(\(mp3\)\|\(flac\)\|\(ogg\)|\(ogg\)|\(wma\)\)$' | grep -E albums/ > $TMP_CACHE # albums only
-mv $TMP_CACHE $CACHE
-rm $TMP_CACHE
+find "${STORE_PREFIX}" -iregex '^.*\.\(\(mp3\)\|\(flac\)\|\(ogg\)|\(ogg\)|\(wma\)\)$' | grep -E albums/ > $CACHE # albums only
 
 echo "finished writing csv too \"${CACHE}\" ..."
 echo 'transforming csv ...'
@@ -26,15 +23,23 @@ echo 'transforming csv ...'
 sed -E -i "s/^.*music-lib\///g" ${CACHE}
 # normalize, double quote all field values
 sed -E -i 's/^(albums)\/(.*)\/([0-9]{4}) - (.*)\/(([0-9]{1,2}).)?([0-9]{2})\. (.*)\.(.*)$/"\1"\/"\3"\/"\2"\/"\4"\/"\6"\/"\7"\/"\8"\/"\9"\/""\/""\/""\/""\/""\/""\/""\/""\/""\/""/g' ${CACHE}
-
-# 1:location/2:artist/3:date/4:album/6:disc/7:track/8:title/9:encoding
+       
+# 1:location/2:artist/3:album/4:date/5:title/6:encoding
 # /10:disc_count/11:track_count/12:genre/13:lyrics/14:file/15:file_size:16:bitrate/17:art/18:create_ts/19:update_ts
+
+# echo "searching for singles ..."
+
+TIME_STAMP="$(date.sh)"
+CACHE="${CONFIG_PREFIX}/${TIME_STAMP}_cache.m3u"
+find "${STORE_PREFIX}" -iregex '^.*\.\(\(mp3\)\|\(flac\)\|\(ogg\)|\(ogg\)|\(wma\)\)$' | grep -E singles/ > ${CACHE}
+
+# # remove prefix
+sed -E -i "s/^.*music-lib\///g" ${CACHE}
+sed -E -i 's/^(singles)\/(.*) - (.*) (\([0-9]{4}\)) - (.*)\.(.*)$/"\1"\/"\2"\/"\3"\/"\4"\/"\5"\/"\6"\/""\/""\/""\/""\/""\/""\/""\/""\/""\/""\/""\/""\/""/g' ${CACHE}
+
 # singles
 # "1:location/2:date - 3:album/4:artist - (5:date) - 6:disc.7:track 8:title.9:encoding "
 
-#find "${STORE_PREFIX}" -iregex '^.*\.\(\(mp3\)\|\(flac\)\|\(ogg\)|\(ogg\)|\(wma\)\)$' | grep -E singles/ > "${CACHE}" # albums only
-# TODO
-# sed -E -i              's/^(single)\/(.*)\/([0-9]{4}) - (.*)\/(([0-9]{1,2}).)?([0-9]{2})\. (.*)\.(.*)$/"\1"\/"\3"\/"\2"\/"\4"\/"\6"\/"\7"\/"\8"\/"\9"\/""\/""/g' ${CACHE}
 # sed -E -i      's/^(misc|sountrack)\/(.*)\/([0-9]{4}) - (.*)\/(([0-9]{1,2}).)?([0-9]{2})\. (.*)\.(.*)$/"\1"\/"\3"\/"\2"\/"\4"\/"\6"\/"\7"\/"\8"\/"\9"\/""\/""/g' ${CACHE}
 
 
