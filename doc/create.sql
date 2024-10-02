4e95162f04206a60b7a9eb0b3bf9a7c393bbc5b1
Brian K Preston <brian8181@gmail.com>
2024-10-02 05:19:35 -0500

    working on schema ...

--- doc/create.sql
+++ doc/create.sql
@@ -2,44 +2,28 @@ create schema music;
 
 create table song 
 (
-    id INT PRIMARY KEY AUTOINCREMENT,
-    location TEXT,
-    year TEXT,
-    artist TEXT,
-    album TEXT,
-    album_artist TEXT,
-    track INT,
-    track_count INT,
-    disc INT,
-    disc_count INT,
-    title TEXT,
-    lyrics TEXT,
-    genre TEXT,
-    file_size INT,
-    file TEXT,
-    bitrate INT,
-    encoder TEXT,
-    art TEXT,
-    ext TEXT,
-    update_ts TIMESTAMP,
-    insert_ts TIMESTAMP
+    id INT PRIMARY KEY,                 -- 1
+    location TEXT,                      -- 2
+    year TEXT,                          -- 3
+    artist TEXT,                        -- 4
+    album TEXT,                         -- 5
+    track INT,                          -- 7
+    track_count INT,                    -- 8
+    disc INT,                           -- 9
+    title TEXT,                         -- 10
+    lyrics TEXT,                        -- 11
+    genre TEXT,                         -- 12
+    file TEXT,                          -- 13
+    bitrate INT,                        -- 14
+    encoder TEXT,                       -- 15
+    hash    TEXT,                       -- 16
+    update_ts TIMESTAMP,                -- 16
+    insert_ts TIMESTAMP                 -- 18
 );
-
-create table art 
-(
-	id INT PRIMARY KEY AUTOINCREMENT,
-	file TEXT,
-	type TEXT,
-	hash BLOB,
-	description TEXT,
-	mime_type TEXT,
-	update_ts TIMESTAMP,
-	insert_ts TIMESTAMP
-); 
 
 create table artist 
 (
-    id INT PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY,
     artist TEXT,
     update_ts TIMESTAMP,
     insert_ts TIMESTAMP
@@ -47,10 +31,31 @@ create table artist
 
 create table album 
 (
-    id INT PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY,
     album TEXT,
+    album_artist,
+    disc_count INT,                     -
+    track_count INT,                     
     update_ts TIMESTAMP,
     insert_ts TIMESTAMP
 ); 
 
-.import --csv .cache.csv song
+CREATE TRIGGER song_insert AFTER CREATE 
+ON song
+BEGIN
+   INSERT INTO AUDIT(update_ts, insert_ts_ts) VALUES (datetime('now'), datetime('now'));
+END;
+
+CREATE TRIGGER song_update AFTER UPDATE 
+ON song
+BEGIN
+   INSERT INTO AUDIT(update_ts) VALUES (datetime('now'));
+END;
+
+-- .import --csv .cache.csv song
+-- .mode csv
+-- .separator "/"
+-- select * from song;
+-- select count(*) from song;
+-- select rowid from song;
+-- select id from song;
--- doc/create1.sql
+++ doc/create1.sql
@@ -12,31 +12,31 @@ create table song
 
 create table song 
 (
-    location     TEXT, 
-    artist       TEXT,
-    album        TEXT,
-    album_artist TEXT,
-    year         INTEGER,
-    disc         INTEGER,
-    track        INTEGER,
-    title        TEXT,
-    encoding     TEXT,
-    size         INTEGER,
-    lenght       INTEGER,
-    path         TEXT,
-    update_ts    TEXT,
-    insert_ts    TEXT
+    location     TEXT,          -- 1
+    artist       TEXT,          -- 2
+    album        TEXT,          -- 3
+    album_artist TEXT,          -- 4
+    year         INTEGER,       -- 5
+    disc         INTEGER,       -- 6
+    track        INTEGER,       -- 8
+    title        TEXT,          -- 9
+    encoding     TEXT,          -- 10
+    size         INTEGER,       -- 11
+    lenght       INTEGER,       -- 12
+    path         TEXT,          -- 13
+    update_ts    TEXT,          -- 14
+    insert_ts    TEXT           -- 15
 );
 
 create table artist (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id INTEGER PRIMARY KEY,
 	artist TEXT,
 	update_ts TIMESTAMP,
 	insert_ts TIMESTAMP
 ); 
 
 create table album (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id INTEGER PRIMARY KEY,
 	album TEXT,
 	update_ts TIMESTAMP,
 	insert_ts TIMESTAMP
