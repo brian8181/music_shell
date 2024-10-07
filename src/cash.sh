0e64dd0ac6474b91abf265a4c0ad68d4a7b9a057
Brian K Preston <brian8181@gmail.com>
2024-10-07 18:37:51 -0500

    cash.sh: add destination option ...

--- src/cash.sh
+++ src/cash.sh
@@ -5,7 +5,7 @@
 # FILE_DATE: 'Sun Oct  6 02:08:37 AM CDT 2024'
 # INFO:      'builds a new cash.m3u'
 
-FILE='cash.sh'
+FILE="$0"
 VERSION='0.0.10'
 FILE_DATE='Sun Oct  6 02:08:37 AM CDT 2024'
 FMT_FG_RED='\e[31m'
@@ -19,6 +19,7 @@ DEBUG_MSG="$PRINT_RED_DEBUG: "
 INFO_MSG="$PRINT_GREEN_INFO: "
 VERBOSE=1
 DEBUG=
+DELIMITER='/'
 
 function PRINT_DEBUG
 {
@@ -40,7 +41,7 @@ function INFO
 	  echo ${VERBOSE:+"Date - $FILE_DATE"}
 }
 
-OPTSTRING="d:hpv]"
+OPTSTRING="l:hpvs:d:]"
 while getopts ${OPTSTRING} opt; do
   case ${opt} in
     v)
@@ -48,18 +49,28 @@ while getopts ${OPTSTRING} opt; do
       echo -e "${FMT_FG_GREEN}version 0.0.1${FMT_FG_RED} ${DEBUG:-debug}${FMT_RESET}"
       exit 0
       ;;
-    d)
+    l)
       DELIMITER=${OPTARG}
       echo -e "${FMT_FG_GREEN}DELIMITER=${OPTARG}${FMT_RESET}"
       echo -e "${FMT_FG_RED}delimiter function not yest implented ...${FMT_RESET}"
-      exit 1
+      exit 0
       ;;
     h)
       INFO 
       echo -e "\nUsage: \n" \
-              "$> cash.sh [-[dhpv]] [SRC [DST]]\n" \
-              "version 0.0.1 - $(date)\n"
-              exit 0;
+              "$> cash.sh [-[dhpv]] [SRC [DST]]\n"
+      echo -e "$0 - version 0.0.1 - $(date)\n"
+      exit 0;
+      ;;
+    s)
+      # src
+      INFO 
+      exit 0
+      ;;
+    d)
+      # dst
+      INFO 
+      exit 0
       ;;
     :)
       PRINT_DEBUG "Option -${OPTARG} requires an argument."
@@ -73,8 +84,8 @@ while getopts ${OPTSTRING} opt; do
 done
 
 #### init shell variabls ####
-STORE_PREFIX="${1:-/mnt/music/music-lib}"
-CONFIG_PREFIX="$HOME/.music_shell"
+STORE_PREFIX="${1:-mnt/music/music-lib}"
+CONFIG_PREFIX="${2:-$HOME/.music_shell}"
 TIME_STAMP="$(date.sh)"
 CACHE="${CONFIG_PREFIX}/${TIME_STAMP}_cache.txt"
 
@@ -85,7 +96,7 @@ VALIDATE_RECORD_RXP='/(.*)/(.*)/(.*)/(.*)/(.*)/(.*)/(.*)/(.*)'
 FILE_TYPES_RXP='^.*\.\(\(mp3\)\|\(flac\)\|\(ogg\)|\(ogg\)|\(wma\)|\(m4a\)\)$'
 # source expressions
 #       (1 )  ((3 ) (4       )   ) ((6       )   )(7 )  ((9         ) ) (10      )   (11)   (12)  (13)
-FIELDS'^(.*)\/((.*)/([0-9]{4}) - )|(([0-9]{4}) - )(.*)\/(([0-9]{1,2}).)?([0-9]{2})\. (.*) - (.*)\.(.*)$'
+FIELDS='^(.*)\/((.*)/([0-9]{4}) - )|(([0-9]{4}) - )(.*)\/(([0-9]{1,2}).)?([0-9]{2})\. (.*) - (.*)\.(.*)$'
 #                  (1 )  (2 )  (3       )   (4 )  ((6         )   (7       )   (8 )  (9 ) 
 ALBUM_FIELDS_RXP='^(.*)\/(.*)\/([0-9]{4}) - (.*)\/(([0-9]{1,2}).)?([0-9]{2})\. (.*)\.(.*)$'
 #            (1 )  (2       )   (3 )  ((5         )   (6       )   (7 )   (8  )  (9 ) 
@@ -99,23 +110,24 @@ if [ ! -d $STORE_PREFIX ]; then
     exit 1;
 fi
 
-PRINT_INFO "searching \"${STORE_PREFIX}\", writing cache --> \"${CACHE}\" ..."
 PRINT_INFO "scanning for file types (mp3, flac, ogg, wma, m4a) ..."
+PRINT_INFO "searching \"${STORE_PREFIX}\", writing cache --> \"${CACHE}\" ..."
 find "$STORE_PREFIX" -iregex $FILE_TYPES_RXP > "$CACHE"
 
-PRINT_INFO "tranforming the output ..."
+PRINT_INFO "tranforming the input ..."
 # remove prefix
-sed -E -i "s/^.*music-lib\///g" "$CACHE"
+#sed -Ei "s/${STORE_PREFIX}\///g" "$CACHE"
+sed -Ei "s/^.*music-lib\///g" "$CACHE"
 
 ### albums! ####
 PRINT_INFO "searching for albums ......."
 cat "$CACHE" | egrep "albums/" > "$CACHE"_ALBUMS # albums only
-sed -E -i "s/$ALBUM_FIELDS_RXP/$ALBUMS_FIELDS_REPL_RXP/g" "$CACHE"_ALBUMS
+sed -Ei "s/$ALBUM_FIELDS_RXP/$ALBUMS_FIELDS_REPL_RXP/g" "$CACHE"_ALBUMS
 
 #### misc! & soundtrack ####
 PRINT_INFO "searching for misc & soundtrack ........."
 cat "$CACHE" | egrep "(misc/)|(soundtrack/)" > "$CACHE"_MISC
-sed -E -i "s/$FIELDS_RXP/$FIELDS_REPL_RXP/g" "$CACHE"_MISC
+sed -Ei "s/$FIELDS_RXP/$FIELDS_REPL_RXP/g" "$CACHE"_MISC
 
 #### albums, singles, misc, sondtrack! ####
 cat "$CACHE"_ALBUMS "$CACHE"_MISC | egrep $VALIDATE_RECORD_RXP > "$CACHE"
