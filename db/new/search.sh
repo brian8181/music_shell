481af3a3f3eafc9804a545ff3679b32a7fa980e7
Brian K Preston <brian8181@gmail.com>
2024-11-05 17:12:07 -0600

    update ...

--- db/new/search.sh
+++ db/new/search.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
-QUERY="select file from song where title like '%$1%'" 
-sqlite3 music.db "$QUERY" > .SEARCH_TEXT
+SEARCH="$1"
+SQL="select file from song where title like '%$SEARCH%'" 
+
+sqlite3 music.db "$SQL" > .SEARCH_TEXT
 cat -n .SEARCH_TEXT 
 echo "found $(cat .SEARCH_TEXT | wc -l) ..." 
