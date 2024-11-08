3e0d27e30d54a11d9865415ea8947a6b569372cb
Brian K Preston <brian8181@gmail.com>
2024-11-08 06:16:59 -0600

    colorfiy working ...

--- shell/colorfiy.sh
+++ shell/colorfiy.sh
@@ -34,7 +34,13 @@ echo "test"
 echo "${FMT_FG_GREEN}testing 123...${FMT_RESET}"
 
 DST_FILE=${FILE%*.???}.fmt.${FILE##*.}
-cat $FILE | sed -E "s/([^\|]*)/${FMT_FG_GREEN}<\1>${FMT_RESET}/g" > "$DST_FILE"
+cat $FILE | sed -E "s/([^\|]*)/${FMT_FG_GREEN}\1${FMT_RESET}/1; s/([^\|]*)/${FMT_FG_YELLOW}\1${FMT_RESET}/2; s/([^\|]*)/${FMT_FG_YELLOW}\1${FMT_RESET}/3; \
+                    s/([^\|]*)/${FMT_FG_CYAN}\1${FMT_RESET}/4; s/([^\|]*)/${FMT_FG_LIGHT_GREY}\1${FMT_RESET}/5; s/([^\|]*)/${FMT_FG_BLUE}\1${FMT_RESET}/6; \
+                    s/([^\|]*)/${FMT_FG_MAGENTA}\1${FMT_RESET}/5; s/([^\|]*)/${FMT_FG_RED}\1${FMT_RESET}/6; s/([^\|]*)/${FMT_FG_WHITE}\1${FMT_RESET}/7; \
+                    s/([^\|]*)/${FMT_FG_LIGHT_RED}\1${FMT_RESET}/8; s/([^\|]*)/${FMT_FG_LIGHT_BLUE}\1${FMT_RESET}/9; s/([^\|]*)/${FMT_FG_LIGHT_GREEN}\1${FMT_RESET}/10; \
+                    s/([^\|]*)/${FMT_FG_CYAN}\1${FMT_RESET}/11; s/([^\|]*)/${FMT_FG_LIGHT_GREY}\1${FMT_RESET}/12; s/([^\|]*)/${FMT_FG_BLUE}\1${FMT_RESET}/13; \
+                    s/([^\|]*)/${FMT_FG_MAGENTA}\1${FMT_RESET}/14";
+                     > "$DST_FILE"
 
 while read line; do
 	echo -e "$line"
