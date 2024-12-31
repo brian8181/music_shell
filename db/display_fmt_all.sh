baaf8b1a81b6ac57bdf9028061654f5ede13071b
Brian K Preston <brian8181@gmail.com>
2024-12-31 03:40:29 -0600

    implemented diplay_fmt_all.sh NO_COLOR option & removed color.sh (it eblongs with script project) ...

--- db/display_fmt_all.sh
+++ db/display_fmt_all.sh
@@ -5,9 +5,9 @@
 # FILE_DATE: Mon Dec  2 03:27:12 PM CST 2024
 # INFO: dislay formated text from sqlite
 
-COLOR_OPT=
+NO_COLOR=
 
-OPTSTRING="abch"
+OPTSTRING="abnh"
 while getopts ${OPTSTRING} opt; do
   case ${opt} in
     a)
@@ -16,8 +16,8 @@ while getopts ${OPTSTRING} opt; do
     b)
       echo "Option -b was triggered."
       ;;
-    c)
-      COLOR_OPT="TRUE";
+    n)
+      NO_COLOR="TRUE";
       ;;
      h)
       # display help
@@ -30,18 +30,21 @@ while getopts ${OPTSTRING} opt; do
 done
 shift $(($OPTIND-1))
 
-if [[ -z "$COLOR_OPT" ]]; then
-    echo "todo (color) ..."
-fi
-
 source $HOME/bin/color.sh
 WHERE=$1
 LIMIT=$2
-FORMAT="$(PrintGreen %s) $(PrintBright $(PrintWhite :)) $(PrintMagenta %d) $(PrintBright $(PrintWhite -)) $(PrintRed %s) $(PrintBright $(PrintWhite :)) $(PrintBlue %0d.%02d). $(PrintCyan %s) $(PrintBright $(PrintWhite -\>)) $(PrintGrey %s)"
-DEBUG="SELECT format('$FORMAT', artist, year, album, disc, track, title, file) FROM cash WHERE location=='albums' ${WHERE:+" and $WHERE"} ORDER BY artist, year, album, disc, track, title${LIMIT:+" LIMIT $LIMIT"};"
-echo $DEBUG
 
-# albums only, sorted
-sqlite3 ~/db/music.db "$DEBUG"
+if [[ -z "$NO_COLOR" ]]; then
+      FMT_GREEN_STR='$(PrintGreen %s)'
+      FORMAT="$(PrintGreen %s) $(PrintBright $(PrintWhite :)) $(PrintMagenta %d) $(PrintBright $(PrintWhite -)) $(PrintRed %s) $(PrintBright $(PrintWhite :)) $(PrintBlue %0d.%02d). $(PrintCyan %s) $(PrintBright $(PrintWhite -\>)) $(PrintGrey %s)"
+      DEBUG="SELECT format('$FORMAT', artist, year, album, disc, track, title, file) FROM cash WHERE location=='albums' ${WHERE:+" and $WHERE"} ORDER BY artist, year, album, disc, track, title${LIMIT:+" LIMIT $LIMIT"};"
+      echo $DEBUG
+      # albums only, sorted
+      sqlite3 ~/db/music.db "$DEBUG"
+    else
+      sqlite3 ~/db/music.db "SELECT format('%s : %d - %s : %0d.%02d. %s -> %s', artist, year, album, disc, track, title, file) FROM cash WHERE location=='albums' ${WHERE:+" and $WHERE"} ORDER BY artist, year, album, disc, track, title ${LIMIT:+" LIMIT $LIMIT"};"
+fi
+
+
 
 
