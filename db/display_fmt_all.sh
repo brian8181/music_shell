459e637f962ecd8ad3b7b9c36385ef0b5b8cf306
Brian K Preston <brian8181@gmail.com>
2025-01-15 08:11:32 -0600

    add display_fmt_all.conf ...

--- db/display_fmt_all.sh
+++ db/display_fmt_all.sh
@@ -43,15 +43,7 @@ function PrintDefault
 if [[ -z "$NO_COLOR" ]]; then
       FMT_GREEN_STR='$(PrintGreen %s)'
 
-      ARTIST="$(PrintGreen %s)"
-      YEAR="$(PrintMagenta %d)"
-      ALBUM="$(PrintRed %s)"
-      DISC_TRACK="$(PrintYellow %0d.%02d.)"
-      TITLE="$(PrintCyan %s)"
-      FILE="$(PrintGrey %s)"
-      SEPARATOR1="$(PrintDefault :)"
-      SEPARATOR2="$(PrintDefault -)"
-      SEPARATOR3="$(PrintDefault ->)"
+      source display_fmt _all.conf
 
       FORMAT="$ARTIST $SEPARATOR1 $YEAR $SEPARATOR2 $ALBUM $SEPARATOR1 $DISC_TRACK $TITLE $SEPARATOR3 $FILE"
       DEBUG="SELECT format('$FORMAT', artist, year, album, disc, track, title, file) FROM cash WHERE location=='albums' ${WHERE:+" and $WHERE"} ORDER BY artist, year, album, disc, track, title${LIMIT:+" LIMIT $LIMIT"};"
